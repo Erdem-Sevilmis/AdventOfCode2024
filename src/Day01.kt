a5aba7efc3d8317fd36f0197453cf791fdb2df57
Erdem Sevilmis <erdem.sevilmis@erstegroup.com>
2024-12-02 11:44:38 +0100

    Day 1, pt2

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,7 +10,17 @@ fun day01() {
         rightList.add(right)
     }
     "Day 01: ${calculateTotalDistance(leftList, rightList)}".println()
+    "Day 01 pt2: ${calculateSimilarityScore(leftList, rightList)}".println()
 }
 
 fun calculateTotalDistance(leftList: List<Int>, rightList: List<Int>) =
     leftList.sorted().zip(rightList.sorted()).sumOf { (l, r) -> kotlin.math.abs(l - r) }
+
+fun calculateSimilarityScore(leftList: List<Int>, rightList: List<Int>): Int {
+    val rightListCountMap = rightList.groupingBy { it }.eachCount()
+
+    return leftList.sumOf { number ->
+        val countInRightList = rightListCountMap[number] ?: 0
+        number * countInRightList
+    }
+}
